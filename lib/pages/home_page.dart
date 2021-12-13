8696e459969d50382c013b92b978ee21207ea1fb
Felipe Carvalho <feliped3carvalho@gmail.com>
2021-12-13 16:51:57 -0300

    app rodando no linux e home style final

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:projeto01/controllers/home_controller.dart';
 import 'package:projeto01/models/post_model.dart';
-import 'package:projeto01/repositories/home_repository_mock.dart';
+import 'package:projeto01/repositories/home_repository_imp.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,7 +11,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final HomeController _controller = HomeController(HomeRepositoryMock());
+  final HomeController _controller = HomeController(HomeReposityImp());
 
   @override
   void initState() {
@@ -22,14 +22,25 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Home',
+        ),
+      ),
       body: ValueListenableBuilder<List<PostModel>>(
         valueListenable: _controller.posts,
         builder: (_, list, __) {
-          return ListView.builder(
+          return ListView.separated(
             itemCount: list.length,
             itemBuilder: (_, index) => ListTile(
-              title: Text(list[index].title),
+              leading: Text(list[index].id.toString()),
+              title: Text(
+                list[index].title,
+              ),
+              trailing: Icon(Icons.arrow_forward),
             ),
+            separatorBuilder: (context, index) => Divider(),
           );
         },
       ),
